e6a2639764e512b4a9ba0e2c820122fb2d903e2e
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-21 08:48:21 +0545

    Fix: Add missing line breaks in type output for better readability

--- BIM/Lab Work/Lab-6/primitivetype.js
+++ BIM/Lab Work/Lab-6/primitivetype.js
@@ -3,7 +3,7 @@ const val = 12;
 const isEmpty = true;
 // const json = { name: "anuj" };
 
-document.writeln(`"${word}" type - ${typeof word}`);
-document.writeln(`${val} type - ${typeof val} `);
-document.writeln(`${isEmpty} type - ${typeof isEmpty} `);
+document.writeln(`"${word}" type - ${typeof word}<br>`);
+document.writeln(`${val} type - ${typeof val}<br>`);
+document.writeln(`${isEmpty} type - ${typeof isEmpty}<br>`);
 // document.writeln(`${json} type - ${typeof JSON} `);
