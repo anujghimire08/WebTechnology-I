7d93663ac4cbeb672c492f1de0b335dfb6b12a6f
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-03 12:30:38 +0545

    Refactor DifferentFunctions.js to improve formatting and add new functions

--- BIM/Lab Work/Lab-7/DifferentFunctions.js
+++ BIM/Lab Work/Lab-7/DifferentFunctions.js
@@ -3,16 +3,27 @@ function hello(){
   alert("Hello");
 }
 hello();
+
 // function with parameter
 function hello(name){
   alert(`Hello${name}`);
 }
 hello("anuj");
+
 // unnamed function without parameter
 const hello1 = ()=> alert("hello");
 hello1();
+
 // unnamed function with parameter
 const hello2 = (number)=>{
   alert(number);
 }
 hello2(38744389);
+
+// function return
+const randomNumber = ()=> Math.random();
+  
+// function with default value
+function bye(name ="Anuj"){
+  console.log(name);
+}
