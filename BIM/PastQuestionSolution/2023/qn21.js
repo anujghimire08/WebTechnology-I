58ee01315ba95d4b2c7f4720738368ec12ca511e
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-07 10:32:55 +0545

    QN.21 [September - October 2023]

---
+++ BIM/PastQuestionSolution/2023/qn21.js
@@ -0,0 +1,43 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit",(e)=>{
+e.preventDefault();
+const userId = document.getElementById("userid").value.trim();
+const phone = document.getElementById("phone").value.trim();
+const mail = document.getElementById("email").value.trim();
+const password = document.getElementById("password").value.trim();
+const cpassword = document.getElementById("confirm").value.trim();
+
+// user id validation
+if(isNaN(userId)|| !userId){
+  alert("userid must be number");
+  return;
+}
+
+// phone number validation
+if(!/^\d{10}$/.test(phone)){
+  alert("phone length must be 10");
+  return;
+}
+
+// email validation
+if(!/^[a-zA-Z0-9._%+-]+@gmail\.com(\.[a-z]{2,})?$/.test(mail)){
+  alert("invalid email format");
+  return;
+}
+
+// password validation
+if(password.length < 6){
+  alert("password length must be at least 6");
+  return;
+}
+
+// confirm passsword validation
+if(password!==cpassword){
+  alert("password don't matched");
+  return;
+}
+
+alert("form validated!");
+form.reset();
+})
