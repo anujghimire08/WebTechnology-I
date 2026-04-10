5385b342c9b5fba8e898031083fb4b9bd08e5a8d
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-10 17:21:33 +0545

    QN.18 [May - 2025]

--- BIM/PastQuestionSolution/2025/qn18.js
+++ BIM/PastQuestionSolution/2025/qn18.js
@@ -2,15 +2,13 @@ let target = Number(prompt("Enter Number To Search"));
 let arr = [10, 20, 30, 50, 50];
 const search = (arr, target) => {
   let isFound = false;
-  let index;
-  arr.forEach((el, idx) => {
+  arr.forEach((el) => {
     if (el === target) {
       isFound = true;
-      index = idx;
     }
   });
   isFound
-    ? console.log(`${target} Found at index ${index}`)
-    : console.log(`${target} Not Found`);
+    ? console.log(`${target} Value is in array`)
+    : console.log(`${target} value not found in array`);
 };
 search(arr, target);
