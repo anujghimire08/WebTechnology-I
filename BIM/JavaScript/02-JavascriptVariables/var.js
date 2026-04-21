0b2e4ba13a24baff2fcc1e1a717b07e89bdd4aa9
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:45:44 +0545

    Add demonstration of variable declaration using var with comments

---
+++ BIM/JavaScript/02-JavascriptVariables/var.js
@@ -0,0 +1,14 @@
+function demo() {
+  if (true) {
+    var a = 5;
+  }
+  console.log(a); // accessed
+}
+
+/**
+ * older way of defining variable
+ * Can change value
+ * Can be redeclared
+ * Function scoped
+ * visible inside whole function
+ */
