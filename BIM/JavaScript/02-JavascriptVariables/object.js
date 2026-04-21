2c7324b43bb9555f587a00226edc20411018dc30
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:58:41 +0545

    Add object declaration example with properties and console logs

---
+++ BIM/JavaScript/02-JavascriptVariables/object.js
@@ -0,0 +1,11 @@
+// An object is a group of values
+const myObject = {};
+myObject.john = { firstname: "John", lastname: "Doe", fullname: "John Doe" };
+myObject.anuj = {
+  firstname: "Anuj",
+  lastname: "Ghimire",
+  fullname: "Anuj Ghimire",
+};
+
+console.log(myObject.anuj.firstname);
+console.log(myObject);
