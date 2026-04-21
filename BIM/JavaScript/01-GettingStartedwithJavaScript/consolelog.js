5220fdb64c7bdac3fdef65dcaca363fd0fe0c5c4
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:30:07 +0545

    Add console log demonstration script with comments and examples

---
+++ BIM/JavaScript/01-GettingStartedwithJavaScript/consolelog.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+/*
+The most common of these methods is console. log().
+
+In a browser environment, the console. log() function is predominantly used for debugging purposes.
+
+the console. log() function prints Hello, World! to the console and returns undefined because console. log() has no explicit return value.
+*/
+
+var greet = "Hello",
+  who = "World";
+console.log("%s, %s!", greet, who);
