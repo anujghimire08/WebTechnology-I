cb7729d413f96b50089b10b03852f584c2ba6481
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:32:23 +0545

    Add window alert demonstration script with comments and logging

---
+++ BIM/JavaScript/01-GettingStartedwithJavaScript/windowalert.js
@@ -0,0 +1,7 @@
+/* The alert method is technically a property of window object, but since all window properties are automatically global variables, we can use alert as a global variable instead of as a property of window -meaning you can directly use alert() instead of window.alert().
+
+The alert method displays a visual alert box on screen. The alert method parameter is displayed to the user in
+plain text
+ */
+alert("Pause!");
+console.log("Alert was dismissed");
