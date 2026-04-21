0fbe0b6d19769b5eaa8d9622e44f0ee2b8584678
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:34:23 +0545

    Add window prompt demonstration script with comments

---
+++ BIM/JavaScript/01-GettingStartedwithJavaScript/windowprompt.js
@@ -0,0 +1,4 @@
+// An easy way to get an input from a user is by using the prompt() method. SYNTAX:prompt(text, [default]);
+
+let age = prompt("How old are you?");
+console.log(age); // Prints the value inserted by the user
