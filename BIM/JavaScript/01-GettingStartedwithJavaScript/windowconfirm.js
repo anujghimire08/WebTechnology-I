25353223e07e62ccde6540c7daaf8f2ed7d9564f
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:38:26 +0545

    Add window confirm demonstration script with comments

---
+++ BIM/JavaScript/01-GettingStartedwithJavaScript/windowconfirm.js
@@ -0,0 +1,4 @@
+// The window.confirm() method displays a modal dialog with an optional message and two buttons, OK and Cancel.
+
+let deleteConfirm = window.confirm("Are you sure you want to delete this?");
+deleteConfirm ? alert("true") : alert("false");
