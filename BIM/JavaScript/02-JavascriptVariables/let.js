c46bd195c26ba554d5aa752ccff4debd3afa1fb9
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-21 22:46:59 +0545

    Add demonstration of variable declaration using let with comments

---
+++ BIM/JavaScript/02-JavascriptVariables/let.js
@@ -0,0 +1,14 @@
+function demo() {
+  if (true) {
+    let b = 5;
+  }
+  console.log(b); // error
+}
+
+/**
+ * modern way of defining variable
+ * Value can change
+ * Cannot redeclare in same scope
+ * Block scoped { }
+ * recommended for changing values
+ */
