b9ccaa4eb523ecb632b081bd78002fe1e8873683
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-22 11:59:21 +0545

    Add null.js to explain the null value and its type

---
+++ BIM/JavaScript/03-BuiltInConstants/null.js
@@ -0,0 +1,7 @@
+/*null is used for representing the intentional absence of an object value and is a primitive value. Unlike undefined, it is not a property of the global object.
+
+The typeof null is 'object'.
+ */
+
+let a = null;
+console.log(a);
