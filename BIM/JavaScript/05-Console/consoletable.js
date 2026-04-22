0421c714c3affdbbcade08bb0a86b538dcfeffbc
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-22 12:59:01 +0545

    Add consoletable.js to demonstrate usage of console.table() for displaying objects and arrays

---
+++ BIM/JavaScript/05-Console/consoletable.js
@@ -0,0 +1,10 @@
+// console.table() can be used to display objects and arrays in a tabular format.
+
+const arr = ["Apple", "Banana", "Mango"];
+console.table(arr);
+
+const students = [
+  { name: "Anuj", age: 20 },
+  { name: "Ram", age: 22 },
+];
+console.table(students);
