213a783f4b6ce8f850e8fe1401c1090fcf4b37a1
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-23 15:10:12 +0545

    Add trim.js to demonstrate whitespace trimming in JavaScript

---
+++ BIM/JavaScript/07-String/trim.js
@@ -0,0 +1,6 @@
+// To trim whitespace from the edges of a string, use String.prototype.trim:
+
+let a = "    sdbsdd    dsd      ssdds   ";
+console.log(a.trimEnd());
+console.log(a.trimStart());
+console.log(a.trim());
