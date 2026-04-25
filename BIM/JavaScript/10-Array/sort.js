a4a47a6a94912433441745d26891edd4c88967cb
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-25 20:00:41 +0545

    add sorting functionality to array

---
+++ BIM/JavaScript/10-Array/sort.js
@@ -0,0 +1,4 @@
+let arr = [1, 36, 89, 343, 2, 0, 4, 9, 3];
+for (let val of arr) console.log(val);
+arr.sort((a, b) => a - b); // a-b ascending and b-a descending order;
+for (let val of arr) console.log(val);
