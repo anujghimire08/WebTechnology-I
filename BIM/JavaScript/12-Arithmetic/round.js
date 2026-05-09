e38ea507d2199c794ae78ba4712ddc97404316de
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-05-09 07:53:29 +0545

    Arithmetic Rounding

---
+++ BIM/JavaScript/12-Arithmetic/round.js
@@ -0,0 +1,15 @@
+var a = Math.round(2.3); console.log(a); // 2
+var b = Math.round(2.7); console.log(b); // 3
+var c = Math.round(2.5); console.log(c); // 3
+var d = Math.round(-2.7);console.log(d); //-3
+var e = Math.round(-2.5);console.log(e); //-2
+
+
+
+
+var a = Math.ceil(2.3);        // a is now 3
+var b = Math.ceil(2.7);        // b is now 3
+
+
+var a = Math.floor(2.3);        // a is now 2
+var b = Math.floor(2.7);        // b is now 2
