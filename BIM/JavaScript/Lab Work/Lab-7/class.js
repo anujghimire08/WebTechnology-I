2004034fb52196f5c17b3997ca508d14ce049e91
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-05-14 23:05:39 +0545

    class js

--- BIM/JavaScript/Lab Work/Lab-7/class.js
+++ BIM/JavaScript/Lab Work/Lab-7/class.js
@@ -1,18 +1,18 @@
-class person{
+class person {
   #phone;
-  constructor(name,address,phone){
+  constructor(name, address, phone) {
     this.name = name;
-      this.address = address;
-      this.#phone = phone;
+    this.address = address;
+    this.#phone = phone;
   }
   fullinfo = () => {
-     console.log(this.name + "," + this.address);
-  }
-  getNumber(){
+    console.log(this.name + "," + this.address);
+  };
+  getNumber() {
     console.log(this.#phone);
   }
 }
 
-const p1 = new person("anuj","BRT",6232832);
+const p1 = new person("anuj", "BRT", 6232832);
 p1.fullinfo();
 p1.getNumber();
